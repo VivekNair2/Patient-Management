f85dd60a4e9f63cab96c53ff69a8366c2c908305
VivekNair2 <vivekh2004@gmail.com>
2025-10-10 19:30:36 +0530

    grpc logic started

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -4,6 +4,8 @@ import com.pm.patientservice.dto.PatientRequestDto;
 import com.pm.patientservice.dto.PatientResponseDto;
 import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
 import com.pm.patientservice.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name="Patient",description ="Api for managing patients")
 public class PatientController {
     public final PatientService patientService;
 
@@ -21,12 +24,14 @@ public class PatientController {
     }
 
     @GetMapping
+    @Operation(summary="Get all patients",description="Retrieve a list of all patients")
     public ResponseEntity<List<PatientResponseDto>> getPatients() {
         List<PatientResponseDto> patients=patientService.getPatients();
         return ResponseEntity.ok().body(patients);
     }
 
     @PostMapping
+    @Operation(summary="Create a new patient",description="Create a new patient with the provided details")
     public ResponseEntity<PatientResponseDto> createPatient(@Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDto patientRequestDto) {
         PatientResponseDto savedPatient=patientService.createPatient(patientRequestDto);
         return ResponseEntity.ok().body(savedPatient);
@@ -34,6 +39,7 @@ public class PatientController {
 
 
     @PutMapping("/{id}")
+    @Operation(summary="Update an existing patient",description="Update the details of an existing patient by ID")
     public ResponseEntity<PatientResponseDto> updatePatient(@PathVariable("id") UUID patientId,
                                                             @Validated({Default.class}) @RequestBody PatientRequestDto patientRequestDto) {
         PatientResponseDto updatedPatient=patientService.updatePatient(patientId, patientRequestDto);
@@ -41,6 +47,7 @@ public class PatientController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary="Delete a patient",description="Delete an existing patient by ID")
     public ResponseEntity<Void> deletePatient(@PathVariable("id") UUID patientId) {
         patientService.deletePatient(patientId);
         return ResponseEntity.noContent().build();
