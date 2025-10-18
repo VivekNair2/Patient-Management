4715cd9ad0b5e1582b056e605b5845e23bafd104
VivekNair2 <vivekh2004@gmail.com>
2025-10-18 19:11:32 +0530

    Authservice controller logic done

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -1,4 +1,21 @@
 package com.pm.authservice.service;
 
+import com.pm.authservice.dto.LoginRequestDTO;
+import org.springframework.stereotype.Service;
+import com.pm.authservice.model.User;
+import java.util.Optional;
+
+@Service
 public class AuthService {
+    private final UserService userService;
+    public AuthService(UserService userService) {
+        this.userService = userService;
+    }
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO){
+        // Dummy implementation for illustration
+        Optional<String> token=userService.findByEmail(loginRequestDTO.getEmail())
+                               .filter(u->passwordEncoder.matches(loginRequestDTO.getPassword(),u.getPassword()))
+                               .map(u->jwtUtil.generateToken(u.getEmail(),u.getRole()));
+        return token;
+    }
 }
